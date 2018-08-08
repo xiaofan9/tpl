cf0573d7ea9725b324dea7fe0c1cb5f42c3e2f5d
sifan <718662552@qq.com>
2018-08-08 10:20:55 +0800

    fixed 优化下写法,增加大量注释

--- index.js
+++ index.js
@@ -1,285 +1,314 @@
 (function(w) {
-    const tagsToReplace = {
-        "&amp;": "&",
-        "&lt;": "<",
-        "&gt;": ">"
-    };
+  const RMSCRIPT = /<script.*?>.*?<\/script>/gi;
+  const RMLINE = /[\r\n]/g;
+  const STARTTAG = /{{|{%/;
+  const CONDITION = /\((.*?)\)/;
 
-    function tpl(param) {
-        let html = document
-            .querySelector("body")
-            .innerHTML.replace(/[\r\n]/g, "")
-            .replace(/<script.*?>.*?<\/script>/gi, "");
+  const tagsToReplace = {
+    "&amp;": "&",
+    "&lt;": "<",
+    "&gt;": ">"
+  };
 
-        let blockStack = [];
-        let blockID = 0;
-        let astTree = [];
+  w.tpl = tpl;
 
-        param._s = str => str;
-        param._f = (name, str) => name(str);
+  function tpl(param) {
+    let body = document.querySelector("body");
+    let html = body.innerHTML.replace(RMLINE, "").replace(RMSCRIPT, "");
 
-        function parse(html = "") {
-            // 匹配正则
-            const startMatchs = html.match(/{{|{%/) || {};
-            const isBlock = startMatchs[0] === "{%";
-            const startIndex = startMatchs.index;
+    // 块级栈
+    let blockStack = [];
+    let astTree = [];
 
-            if (startIndex !== 0) {
-                let len = html.length;
-                let text = html.slice(0, startIndex || len);
+    // 解析变量函数
+    param._s = str => str;
 
-                if (text.trim()) {
-                    let obj = {
-                        type: 1,
-                        text
-                    };
-                    if (blockStack.length) {
-                        if (!blockStack[blockStack.length - 1].children) {
-                            blockStack[blockStack.length - 1].children = [];
-                        }
+    // 过滤器处理函数
+    param._f = (name, str) => name(str);
 
-                        blockStack[blockStack.length - 1].children.push(obj);
-                    } else {
-                        astTree.push(obj);
-                    }
-                }
+    // 编译html字符串为ast语法树
+    function parse(html = "") {
+      // html 为空时
+      if (!html.trim()) {
+        blockStack = [];
 
-                if (html.slice(startIndex || len).length) {
-                    return parse(html.slice(startIndex || len));
-                }
+        if (html.length) {
+          return " ";
+        }
+        return "";
+      }
+      // 匹配正则
+      const startMatchs = html.match(STARTTAG) || {};
 
-                return;
+      // 是否是块级作用域
+      const isBlock = startMatchs[0] === "{%";
+      const startIndex = startMatchs.index;
+
+      if (startIndex !== 0) {
+        let len = html.length;
+        let text = html.slice(0, startIndex || len);
+
+        if (text) {
+          let ast = { type: 1, text };
+
+          // 存在块级表明该文本节点在块级中
+          if (blockStack.length) {
+            if (!blockStack[blockStack.length - 1].children) {
+              blockStack[blockStack.length - 1].children = [];
             }
 
-            const endIndex = html.indexOf(isBlock ? "%}" : "}}");
+            blockStack[blockStack.length - 1].children.push(ast);
+          } else {
+            astTree.push(ast);
+          }
 
-            if (endIndex === -1) {
-                astTree.push({
-                    type: 1,
-                    text: html.slice(0, 2)
-                });
+          if (html.slice(startIndex || len).length) {
+            return parse(html.slice(startIndex || len));
+          }
+        }
 
-                if (html.slice(2).length) {
-                    return parse(html.slice(2));
-                }
+        blockStack = [];
+
+        return;
+      }
+
+      const endIndex = html.indexOf(isBlock ? "%}" : "}}");
+
+      // html 只有 {{ 或 {% 时，没有 }} 或 %} 处理语句
+      if (endIndex === -1) {
+        let ast = { type: 1, text: html.slice(0, 2) };
+
+        if (blockStack.length) {
+          if (!blockStack[blockStack.length - 1].children) {
+            blockStack[blockStack.length - 1].children = [];
+          }
+
+          blockStack[blockStack.length - 1].children.push(ast);
+        } else {
+          astTree.push(ast);
+        }
+
+        if (html.slice(2).length) {
+          return parse(html.slice(2));
+        }
+
+        return;
+      }
+
+      // 块级作用域
+      if (isBlock) {
+        // 拿到{% %} 里面的东西
+        let item = html
+          .trim()
+          .slice(startIndex + 2, endIndex)
+          .trim();
+        let block = "";
+        let isElse = false;
+
+        // end结束
+        if (item === "}") {
+          // 出栈
+          let last = blockStack.pop();
+
+          if (blockStack.length) {
+            if (!blockStack[blockStack.length - 1].children) {
+              blockStack[blockStack.length - 1].children = [];
             }
 
-            if (isBlock) {
-                // 块级作用域
-                let item = html
-                    .trim()
-                    .slice(startIndex + 2, endIndex)
-                    .trim();
-                let block = "";
-                let isElse = false;
+            blockStack[blockStack.length - 1].children.push(last);
+          } else {
+            astTree.push(last);
+          }
 
-                // 结束
-                if (item === "}") {
-                    // 出栈
-                    let last = blockStack.pop();
+          return parse(html.slice(endIndex + 2));
+        }
 
-                    if (blockStack.length) {
-                        if (!blockStack[blockStack.length - 1].children) {
-                            blockStack[blockStack.length - 1].children = [];
-                        }
+        // 拿到内部的是for 还是 if
+        if (item.indexOf("if") !== -1 || item.indexOf("else") !== -1) {
+          block = "if";
+          if (item.indexOf("if") !== 0) {
+            block = "else if";
+            isElse = true;
+          }
+        } else if (item.includes("for")) {
+          block = "for";
+        }
+        // 拿到条件语句
+        let matchs = [...new Set(item.match(CONDITION))];
 
-                        blockStack[blockStack.length - 1].children.push(last);
-                    } else {
-                        astTree.push(last);
-                    }
+        // 对条件语句进行标签替换，innerHTML 拿到的 > < = 默认会被转义
+        item = (matchs[1] || "").replace(/(&lt)?(&gt)?(&amp)?;?/g, replaceTag);
 
-                    return parse(html.slice(endIndex + 2));
-                }
+        let ast = {
+          type: 3,
+          block,
+          item
+        };
 
-                if (item.indexOf("if") !== -1 || item.indexOf("else") !== -1) {
-                    block = "if";
-                    if (item.indexOf("if") !== 0) {
-                        block = "else if";
-                        // else if or else
-                        isElse = true;
-                    }
-                } else if (item.includes("for")) {
-                    block = "for";
-                }
+        // 如果是else 或者 else if
+        if (isElse) {
+          let last = blockStack.pop();
 
-                let matchs = [...new Set(item.match(/\((.*?)\)/))];
+          blockStack.push({
+            ...ast,
+            ...(ast.item ? ast.item : { item: last.item }) // else 拿 if的判断语句，else if 用自己的
+          });
 
-                item = (matchs[1] || "").replace(
-                    /(&lt)?(&gt)?(&amp)?;?/g,
-                    replaceTag
-                );
+          astTree.push(last);
+        } else {
+          blockStack.push(ast);
+        }
+      } else {
+        // {{ 变量
+        let text = html.slice(startIndex + 2, endIndex).trim();
+        // 切割var字串，看有没有过滤器的存在。
+        let tmpVarArr = text.split("|");
 
-                if (isElse) {
-                    let last = blockStack.pop();
+        // 过滤器函数字符串
+        let f = "";
 
-                    blockStack.push({
-                        ...{
-                            type: 3,
-                            block,
-                            id: last.id
-                        },
-                        ...(item ? item : { item: last.item })
-                    });
+        // 变量字串
+        let varStr = tmpVarArr.shift().trim();
 
-                    astTree.push(last);
-                } else {
-                    blockStack.push({
-                        type: 3,
-                        block,
-                        item,
-                        id: blockID,
-                        edit: "start"
-                    });
+        if (tmpVarArr.length) {
+          tmpVarArr.forEach((item, idx) => {
+            let f_ = item.trim(); // 过滤器函数名
+            f = f ? `_f(${f_}, ${f})` : `_f(${f_}, ${varStr})`;
+          });
+        }
 
-                    blockID++;
-                }
+        let ast = {
+          type: 2,
+          item: `{{${text}}}`,
+          expression: f || `_s(${varStr})`
+        };
+
+        if (blockStack.length) {
+          if (!blockStack[blockStack.length - 1].children) {
+            blockStack[blockStack.length - 1].children = [];
+          }
+
+          blockStack[blockStack.length - 1].children.push(ast);
+        } else {
+          astTree.push(ast);
+        }
+      }
+
+      return parse(html.slice(endIndex + 2));
+    }
+
+    // 处理ast 语法，将其转化成html字串
+    function generate() {
+      let html = "";
+
+      astTree.forEach(item => {
+        if (item.type === 1) {
+          html += item.text;
+        } else if (item.type === 2) {
+          html += genVar(item);
+        } else if (item.type === 3) {
+          if (item.block.includes("if")) {
+            html += genIf(item, param);
+          } else {
+            html += genFor(item, param);
+          }
+        }
+      });
+
+      return html;
+    }
+
+    // 处理if 语句
+    function genIf(ast, scope = param) {
+      let str = "";
+
+      let result = new Function("scope", `with(scope) { return ${ast.item} }`)(
+        scope
+      );
+
+      // let varName = ast.item.split(/[=><]/g)[0].trim();
+
+      // let result = new Function(varName, "return " + ast.item)(scope[varName]);
+
+      result = ast.block === "if" ? result : !result;
+
+      // 为假不进行下一步处理
+      if (!result) return str;
+
+      ast.children.forEach(item => {
+        if (item.type === 3) {
+          if (item.block.includes("if")) {
+            str += genIf(item);
+          } else {
+            str += genFor(item);
+          }
+        } else if (item.type === 2) {
+          str += genVar(item);
+        } else {
+          str += item.text;
+        }
+      });
+
+      return str;
+    }
+
+    // 处理变量函数
+    function genVar(ast, scope = param) {
+      let expression = ast["expression"];
+
+      return new Function(
+        "scope",
+        "expression",
+        `with(scope) { return ${expression} }`
+      )(scope, expression);
+    }
+
+    // 处理for循环
+    function genFor(ast, scope = param) {
+      let params = ast.item.split(";").map(i => i.trim());
+      let tmp = params[0].split("=").map(i => i.trim());
+
+      // 解析语句,拿到要用的词句
+      let varName = tmp[0].substring(3).trim();
+      let varVal = ~~tmp[1];
+
+      let condition = ~~params[1].split(/[<>=]={0}/g)[1].trim();
+
+      let way = params[2].substr(params[2].length - 2);
+
+      let str = "";
+
+      for (let i = varVal; i < condition; way === "++" ? i++ : i--) {
+        let scope_;
+        ast.children.forEach(item => {
+          scope_ = { ...scope, ...{ [varName]: i } };
+
+          if (item.type === 3) {
+            if (item.block.includes("if")) {
+              str += genIf(item, scope_);
             } else {
-                // {{ 变量
-                let text = html.slice(startIndex + 2, endIndex).trim();
-                let tmpVarArr = text.split("|");
-
-                let filter = "";
-
-                let varStr = tmpVarArr[0].trim();
-
-                if (tmpVarArr.length > 1) {
-                    tmpVarArr.forEach((item, idx) => {
-                        item = item.trim();
-                        if (idx) {
-                            filter = filter
-                                ? `_f(${item}, ${filter})`
-                                : `_f(${item}, ${varStr})`;
-                        }
-                    });
-                }
-
-                let obj = {
-                    type: 2,
-                    text: `{{${text}}}`,
-                    expression: filter ? filter : `_s(${varStr})`
-                };
-
-                if (blockStack.length) {
-                    if (!blockStack[blockStack.length - 1].children) {
-                        blockStack[blockStack.length - 1].children = [];
-                    }
-
-                    blockStack[blockStack.length - 1].children.push(obj);
-                } else {
-                    astTree.push(obj);
-                }
+              str += genFor(item, scope_);
             }
-
-            return parse(html.slice(endIndex + 2));
-        }
-
-        function compile(astTree) {
-            let str = "";
-
-            astTree.forEach(item => {
-                if (item.type === 1) {
-                    str += item.text;
-                } else if (item.type === 2) {
-                    str += genVar(item);
-                } else if (item.type === 3) {
-                    // if
-                    if (item.block.includes("if")) {
-                        str += genIf(item, param);
-                    } else {
-                        str += genFor(item, param);
-                    }
-                }
-            });
-
-            return str;
-        }
-
-        function genIf(ast, scope = param) {
-            let varN = ast.item.split(/[=><]/g)[0].trim();
-
-            let ifR = new Function(varN, "return " + ast.item)(param[varN]);
-
-            ifR = ast.block === "if" ? ifR : !ifR;
-
-            if (!ifR) return "";
-
-            let str = "";
-
-            ast.children.forEach(item => {
-                if (item.type === 3) {
-                    if (item.block.includes("if")) {
-                        str += genIf(item);
-                    } else {
-                        str += genFor(item);
-                    }
-                } else if (item.type === 2) {
-                    str += genVar(item);
-                } else {
-                    str += item.text;
-                }
-            });
-
-            return str;
-        }
-
-        function genVar(ast, scope = param) {
-            let expression = ast["expression"];
-
-            return new Function(
-                "scope",
-                "expression",
-                `with(scope) { return ${expression} }`
-            )(scope, expression);
-        }
-
-        function genFor(ast, scope = param) {
-            let params = ast.item.split(";");
-            let tmp;
-
-            tmp = params[0].split("=");
-
-            let varName = tmp[0]
-                .trim()
-                .substring(3)
-                .trim();
-            let varVal = ~~tmp[1].trim();
-
-            let condition = ~~params[1].split(/[<>=]={0}/g)[1].trim();
-
-            tmp = params[2].trim();
-            let way = tmp.substr(tmp.length - 2);
-
-            let str = "";
-
-            for (let i = varVal; i < condition; way === "++" ? i++ : i--) {
-                ast.children.forEach(item => {
-                    let tmpScope = { ...scope, ...{ [varName]: i } };
-                    if (item.type === 3) {
-                        if (item.block.includes("if")) {
-                            str += genIf(item, tmpScope);
-                        } else {
-                            str += genFor(item, tmpScope);
-                        }
-                    } else if (item.type === 2) {
-                        str += genVar(item, tmpScope);
-                    } else {
-                        str += item.text;
-                    }
-                });
-            }
-            return str;
-        }
-
-        parse(html);
-
-        let htmlStr = compile(astTree);
-
-        document.querySelector("body").innerHTML = htmlStr;
+          } else if (item.type === 2) {
+            str += genVar(item, scope_);
+          } else {
+            str += item.text;
+          }
+        });
+      }
+      return str;
     }
 
-    function replaceTag(tag) {
-        return tagsToReplace[tag] || tag;
-    }
+    parse(html);
 
-    w.tpl = tpl;
+    console.log(astTree);
+
+    let htmlStr = generate(astTree);
+
+    body.innerHTML = htmlStr;
+  }
+
+  function replaceTag(tag) {
+    return tagsToReplace[tag] || tag;
+  }
 })(window);
